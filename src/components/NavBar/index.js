dce054cfb56c84a2ee4c1ed3e0a794bfd6e7393c
leon-luna-ray <leonluna.ray@gmail.com>
2021-04-26 23:55:57 -0700

    bootstrap working sucessfully

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <Navbar bg="dark" variant="dark"></Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
