bfb688d73000b4b6ba18a9022c6769879283a379
Ray Luna <leon.luna.ray@gmail.com>
2021-04-26 22:24:55 -0700

    restart

--- src/components/ProjectCard.js
+++
@@ -1,9 +0,0 @@
-function ProjectCard() {
-  return (
-    <div className='grid-rows-3'>
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
-
-export default ProjectCard;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// CSS with tailwind
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
