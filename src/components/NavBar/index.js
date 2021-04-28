63877773907a31fd468a5885f3b9d1a288978b21
Ray Luna <leon.luna.ray@gmail.com>
2021-04-27 23:13:29 -0700

    navbar fixed

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Button,
+  Form,
+  NavDropdown,
+} from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <Navbar bg="dark" variant="dark"></Navbar>
-    </div>
+    <>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>Ray Luna</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='#home'>About</Nav.Link>
+            <Nav.Link href='#link'>Projects</Nav.Link>
+            <NavDropdown title='Contact' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 };
 
