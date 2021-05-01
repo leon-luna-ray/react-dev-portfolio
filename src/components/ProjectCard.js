d6d30f7206b244c352cbbaaf8635cc8a4d4d9901
leon-luna-ray <leonluna.ray@gmail.com>
2021-04-30 23:58:32 -0700

    project card fully fuctional

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
 
 const ProjectCard = props => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src={props.imgSrc} />
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.imgSrc} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.text}</Card.Text>
-        <Button variant='primary'>Launch</Button>
+        <Button href={props.appLink} target="_blank" variant="primary">
+          Launch App
+        </Button>
+
+        <Button variant="primary">Github Repo</Button>
       </Card.Body>
     </Card>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,53 +7,65 @@ import madLibz from '../images/madlibz.png';
 
 export const Projects = () => {
   return (
-    <section class='projects-section'>
-          <Row>
-            <Col>
-              <ProjectCard
-                title='Social Butterfly'
-                text='The messaging app of the future.'
-                imgSrc={SocialButterfly}
-              />
-            </Col>
-            <Col>
-              <ProjectCard
-                title='Recipe Book'
-                text='The messaging app of the future.'
-                imgSrc={recipeBook}
-              />
-            </Col>
-            <Col>
-              <ProjectCard
-                title='MadLibz!'
-                text='The messaging app of the future.'
-                imgSrc={madLibz}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <ProjectCard
-                title='Social Butterfly'
-                text='The messaging app of the future.'
-                imgSrc={SocialButterfly}
-              />
-            </Col>
-            <Col>
-              <ProjectCard
-                title='Social Butterfly'
-                text='The messaging app of the future.'
-                imgSrc={SocialButterfly}
-              />
-            </Col>
-            <Col>
-              <ProjectCard
-                title='Social Butterfly'
-                text='The messaging app of the future.'
-                imgSrc={SocialButterfly}
-              />
-            </Col>
-          </Row>
+    <section class="projects-section">
+      <Row>
+        <Col>
+          <ProjectCard
+            title="Social Butterfly"
+            text="The messaging app of the future."
+            imgSrc={SocialButterfly}
+            appLink="https://messenger-app-rsl.herokuapp.com/Login"
+            repoLink=""
+          />
+        </Col>
+        <Col>
+          <ProjectCard
+            title="Recipe Book"
+            text="The messaging app of the future."
+            imgSrc={recipeBook}
+            appLink=""
+            repoLink=""
+          />
+        </Col>
+        <Col>
+          <ProjectCard
+            title="MadLibz!"
+            text="The messaging app of the future."
+            imgSrc={madLibz}
+            appLink=""
+            repoLink=""
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ProjectCard
+            title="Social Butterfly"
+            text="The messaging app of the future."
+            imgSrc={SocialButterfly}
+            appLink=""
+            repoLink=""
+          />
+        </Col>
+        <Col>
+          <ProjectCard
+            title="Social Butterfly"
+            text="The messaging app of the future."
+            imgSrc={SocialButterfly}
+            appLink=""
+            repoLink=""
+          />
+        </Col>
+        <Col>
+          <ProjectCard
+            title="Social Butterfly"
+            text="The messaging app of the future."
+            imgSrc={SocialButterfly}
+            appLink=""
+            repoLink=""
+          />
+        </Col>
+      </Row>
     </section>
   );
 };
