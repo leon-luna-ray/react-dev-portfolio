833334f5c4a9151ff68c8989a5cb322d0b1dc22b
leon-luna-ray <leonluna.ray@gmail.com>
2021-05-01 00:45:49 -0700

    nav links functional

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,23 +5,17 @@ import {
   NavDropdown,
 } from 'react-bootstrap';
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>Ray Luna</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#about'>About</Nav.Link>
-            <Nav.Link href='#projects'>Projects</Nav.Link>
-            <NavDropdown title='Contact' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-            </NavDropdown>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Ray Luna</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href='/'>About</Nav.Link>
+            <Nav.Link href='/projects'>Projects</Nav.Link>
+            <Nav.Link href='/contact'>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
