3072c6dd79fb1c049e67094e462ec88abc10ecc2
Ray Luna <leon.luna.ray@gmail.com>
2021-05-01 13:51:05 -0700

    about section complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,16 @@ import {
   Nav,
   NavDropdown,
 } from 'react-bootstrap';
+import '../style.css';
 
 const NavBar = (props) => {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Ray Luna</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+      <Navbar className='nav-top' expand='lg'>
+        <Navbar.Brand href='#home'>Ray Luna</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
             <Nav.Link href='/'>About</Nav.Link>
             <Nav.Link href='/projects'>Projects</Nav.Link>
             <Nav.Link href='/contact'>Contact</Nav.Link>
