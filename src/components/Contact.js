00e57ca8412b07e1d2f251f42d094d5d70096757
Ray Luna <leon.luna.ray@gmail.com>
2021-05-01 14:27:56 -0700

    contact page functionality working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../style.css';
+import Resume from '../pdf/ray-luna-resume.pdf';
 
 export const Contacts = () => {
+
+  const email = 'mailto:leon.luna.ray@gmail.com';
+  const github = 'https://github.com/leon-luna-ray';
+  const linkedin = 'https://www.linkedin.com/in/ray-l-5b66181b9/';
+
   return (
     <section className='contacts-section'>
-      <Card>
-        <h1>Contact</h1>
-        email: <link href='gat'></link>
-      </Card>
+      <Container>
+        <Row>
+          <Col>
+            <div class='card contact-card text-dark bg-light mb-3'>
+              <div class='card-body'>
+                <h5 class='card-title'>E-Mail </h5>
+                <a href={email}>leon.luna.ray@gmail.com</a>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div class='card contact-card text-dark bg-light mb-3'>
+              <div class='card-body'>
+                <h5 class='card-title'>Github </h5>
+                <a href={github} target='_blank'>
+                  Profile
+                </a>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div class='card contact-card text-dark bg-light mb-3'>
+              <div class='card-body'>
+                <h5 class='card-title'>Linkedin </h5>
+                <a href={linkedin} target='_blank'>
+                  Profile
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div class='card contact-card text-dark bg-light mb-3'>
+              <div class='card-body'>
+                <h5 class='card-title'>E-Mail </h5>
+                <a href={Resume} download='luna-ray-resume'>
+                  Download
+                </a>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,62 +10,62 @@ import readmeGenerator from "../images/readme-generator.png";
 
 export const Projects = () => {
   return (
-    <section class="projects-section">
+    <section class='projects-section'>
       <Row>
         <Col>
           <ProjectCard
-            title="Social Butterfly"
-            text="  This is a messenger app for keeping in touch with friends and family and to me new people! This app allows the user to create an account, search users by username and add new contacts. The public chat allows each user to communicate with other members of the Social Butterfly Community. This app is built on a serverless architecture using React and Firebase."
+            title='Social Butterfly'
+            text='  This is a messenger app for keeping in touch with friends and family and to me new people! This app allows the user to create an account, search users by username and add new contacts. The public chat allows each user to communicate with other members of the Social Butterfly Community. This app is built on a serverless architecture using React and Firebase.'
             imgSrc={SocialButterfly}
-            appLink="https://messenger-app-rsl.herokuapp.com/Login"
-            repoLink="https://github.com/messenger-app-team/messenger-app"
+            appLink='https://messenger-app-rsl.herokuapp.com/Login'
+            repoLink='https://github.com/messenger-app-team/messenger-app'
           />
         </Col>
         <Col>
           <ProjectCard
-            title="Recipe Book"
-            text="An online recipe book to view sand save your favorite recipes. This is a Heroku app built using Node.js, Express, Handlebars, Sequelize, Passport and Materialize. The application allows the user to create an account, add recipes, view recipes, search stored recipes and print the recipe."
+            title='Recipe Book'
+            text='An online recipe book to view sand save your favorite recipes. This is a Heroku app built using Node.js, Express, Handlebars, Sequelize, Passport and Materialize. The application allows the user to create an account, add recipes, view recipes, search stored recipes and print the recipe.'
             imgSrc={recipeBook}
-            appLink="http://arcane-peak-55315.herokuapp.com/login"
-            repoLink="https://github.com/kurtp23/recipe-book"
+            appLink='http://arcane-peak-55315.herokuapp.com/login'
+            repoLink='https://github.com/kurtp23/recipe-book'
           />
         </Col>
         <Col>
           <ProjectCard
-            title="MadLibz!"
-            text="This is a whimsical web application which allows a user create their own MadLibz story by entering a set of words or generated randomly based on part of speech!"
+            title='MadLibz!'
+            text='This is a whimsical web application which allows a user create their own MadLibz story by entering a set of words or generated randomly based on part of speech!'
             imgSrc={madLibz}
-            appLink="https://teammadlibz.github.io/MadLibz/"
-            repoLink="https://github.com/TeamMadLibz/MadLibz/"
+            appLink='https://teammadlibz.github.io/MadLibz/'
+            repoLink='https://github.com/TeamMadLibz/MadLibz/'
           />
         </Col>
       </Row>
       <Row>
         <Col>
           <ProjectCard
-            title="EatDaBurger"
-            text="A fun burger ordering app! The user can order any burger they like. The burger will be saved in their burger queque and moved to the devoured side when the user clicks eat-da-burger! This is a Heroku app build with node.js."
+            title='EatDaBurger'
+            text='A fun burger ordering app! The user can order any burger they like. The burger will be saved in their burger queue and moved to the devoured side when the user clicks eat-da-burger! This is a Heroku app build with node.js.'
             imgSrc={eatDaBurger}
-            appLink="https://burger-app-rluna.herokuapp.com/"
-            repoLink="https://github.com/leon-luna-ray/burgers-app"
+            appLink='https://burger-app-rluna.herokuapp.com/'
+            repoLink='https://github.com/leon-luna-ray/burgers-app'
           />
         </Col>
         <Col>
           <ProjectCard
-            title="Budget Tracker"
-            text="A progressive web app for tracking your budget while travelling and on the go. This app allows the user to enter and track expenses both when the app is online or offline. The offline storage is done using a service worker file and manifest. The app will automatically update the app database as soon as the connection is restored."
+            title='Budget Tracker'
+            text='A progressive web app for tracking your budget while travelling and on the go. This app allows the user to enter and track expenses both when the app is online or offline. The offline storage is done using a service worker file and manifest. The app will automatically update the app database as soon as the connection is restored.'
             imgSrc={budgetTracker}
-            appLink="https://budget-tracker-rluna.herokuapp.com/"
-            repoLink="https://github.com/leon-luna-ray/budget-tracker"
+            appLink='https://budget-tracker-rluna.herokuapp.com/'
+            repoLink='https://github.com/leon-luna-ray/budget-tracker'
           />
         </Col>
         <Col>
           <ProjectCard
-            title="Readme Generator"
-            text="A command line application to seamlessly create a new ReadMe file for your projects quickly! This application will ask you all the right questions to generate a professional quality readme from the command line. As a developer this app saves me a lot of time!"
+            title='Readme Generator'
+            text='A command line application to seamlessly create a new ReadMe file for your projects quickly! This application will ask you all the right questions to generate a professional quality readme from the command line. As a developer this app saves me a lot of time!'
             imgSrc={readmeGenerator}
-            appLink="https://www.youtube.com/watch?v=XI6C2lgj540"
-            repoLink="https://github.com/leon-luna-ray/hw09-readme-generator"
+            appLink='https://www.youtube.com/watch?v=XI6C2lgj540'
+            repoLink='https://github.com/leon-luna-ray/hw09-readme-generator'
           />
         </Col>
       </Row>
