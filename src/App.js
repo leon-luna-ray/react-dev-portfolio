605d18a8266f59dfdf6bf8d05908af8ad75367c2
Ray Luna <leon.luna.ray@gmail.com>
2021-05-01 22:02:07 -0700

    live fuctionality complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import NavBar from './components/NavBar.js';
 import Footer from './components/Footer.js';
 import About from './components/About.js';
@@ -9,25 +9,25 @@ import Contact from './components/Contact.js';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <main>
         <NavBar />
 
-          <div className='page-content'>
-            <Container>
-              <Switch>
-                <Route exact path='/'>
-                  <About />
-                </Route>
-                <Route exact path='/projects'>
-                  <Projects />
-                </Route>
-                <Route exact path='/contact'>
-                  <Contact />
-                </Route>
-              </Switch>
-            </Container>
-          </div>
+        <div className='page-content'>
+          <Container>
+            <Switch>
+              <Route exact path='/'>
+                <About />
+              </Route>
+              <Route exact path='/projects'>
+                <Projects />
+              </Route>
+              <Route exact path='/contact'>
+                <Contact />
+              </Route>
+            </Switch>
+          </Container>
+        </div>
 
         <Row>
           <Footer />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,13 @@ const NavBar = () => {
   return (
     <>
       <Navbar className='nav-top' expand='lg'>
-        <Navbar.Brand href='#home'>Ray Luna</Navbar.Brand>
+        <Navbar.Brand href='#'>Ray Luna</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='mr-auto'>
-            <Nav.Link href='/'>About</Nav.Link>
-            <Nav.Link href='/projects'>Projects</Nav.Link>
-            <Nav.Link href='/contact'>Contact</Nav.Link>
+            <Nav.Link href='#'>About</Nav.Link>
+            <Nav.Link href='#projects'>Projects</Nav.Link>
+            <Nav.Link href='#contact'>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
