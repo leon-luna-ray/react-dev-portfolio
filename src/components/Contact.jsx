f28dc3db9080e8043438d2fe63e68697f57c41e8
Ray Luna <ray@hello-upswell.com>
2023-04-13 23:26:18 -0700

    refactor all pages to pull data from sanity api

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,55 +1,47 @@
 import React from 'react';
+import { fetchProfile } from '../data/sanity';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../style.css';
-import Resume from '../pdf/ray-luna-resume.pdf';
+
+// Todo move this to global state (redux)
+const profile = await fetchProfile();
 
 export const Contacts = () => {
-
-  const email = 'mailto:leon.luna.ray@gmail.com';
-  const github = 'https://github.com/leon-luna-ray';
-  const linkedin = 'https://www.linkedin.com/in/ray-l-5b66181b9/';
-
   return (
     <section className='contacts-section'>
       <Container>
         <Row>
           <Col>
-            <div class='card contact-card text-dark bg-light mb-3'>
-              <div class='card-body'>
-                <h5 class='card-title'>E-Mail </h5>
-                <a href={email}>leon.luna.ray@gmail.com</a>
+            <div className='card contact-card text-dark bg-light mb-3'>
+              <div className='card-body'>
+                <h5 className='card-title'>E-Mail </h5>
+                <a href={`mailto:${profile.email}`}>{profile.email}</a>
               </div>
             </div>
           </Col>
           <Col>
-            <div class='card contact-card text-dark bg-light mb-3'>
-              <div class='card-body'>
-                <h5 class='card-title'>Github </h5>
-                <a href={github} target='_blank'>
-                  Profile
-                </a>
+            <div className='card contact-card text-dark bg-light mb-3'>
+              <div className='card-body'>
+                <h5 className='card-title'>Github </h5>
+                <a href={profile.github} target='_blank'>{profile.github_user}</a>
               </div>
             </div>
           </Col>
         </Row>
         <Row>
           <Col>
-            <div class='card contact-card text-dark bg-light mb-3'>
-              <div class='card-body'>
-                <h5 class='card-title'>Linkedin </h5>
-                <a href={linkedin} target='_blank'>
-                  Profile
-                </a>
+            <div className='card contact-card text-dark bg-light mb-3'>
+              <div className='card-body'>
+                <h5 className='card-title'>Linkedin </h5>
+                <a href={profile.linkedin} target='_blank'>{profile.linkedin_user}</a>
               </div>
             </div>
           </Col>
           <Col>
-            <div class='card contact-card text-dark bg-light mb-3'>
-              <div class='card-body'>
-                <h5 class='card-title'>E-Mail </h5>
-                <a href={Resume} download='luna-ray-resume'>
-                  Download
-                </a>
+            <div className='card contact-card text-dark bg-light mb-3'>
+              <div className='card-body'>
+                <h5 className='card-title'>Website</h5>
+                <a href={profile.website} target='_blank'>{profile.website_name}</a>
               </div>
             </div>
           </Col>
