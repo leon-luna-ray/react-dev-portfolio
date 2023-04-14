97ac05b630b24ddfed88864a19ccb965bfcb9ea8
Ray Luna <ray@hello-upswell.com>
2023-04-13 23:34:44 -0700

    update vite settings for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [reactRefresh()]
 })
