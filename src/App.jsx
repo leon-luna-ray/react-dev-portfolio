57f22a70e8f44e1f99d6af2ae25bfa2cf13ee3c8
Ray Luna <ray@hello-upswell.com>
2023-08-04 11:42:43 -0700

    rm router

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
 import NavBar from "./components/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
-// import About from "./components/About.jsx";
-import Projects from "./components/Projects.jsx";
-import Contact from "./components/Contact.jsx";
-import HomePage from './pages/index.jsx'
+import HomePage from "./pages/index.jsx";
 
 function App() {
   return (
-    <Router>
-      <main>
-        <NavBar />
-        <div className="page-content">
-          <Container>
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route exact path="/projects">
-                <Projects />
-              </Route>
-              <Route exact path="/contact">
-                <Contact />
-              </Route>
-            </Switch>
-          </Container>
-        </div>
-        <Row>
-          <Footer />
-        </Row>
-      </main>
-    </Router>
+    <main>
+      <NavBar />
+      <HomePage />
+      <Footer />
+    </main>
   );
 }
 
