d5275203fa8a241047d96cd276968741b865d105
Ray Luna <leon.luna.ray@gmail.com>
2023-08-04 19:01:34 -0700

    fetch data and set to state

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-
+import { createRoot } from 'react-dom';
 import App from './App';
-
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+
+root.render(
+    <React.StrictMode>
+        <App tab="home" />
+    </React.StrictMode>
+);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,58 +1,54 @@
-import React from "react";
-import { getImageUrl, fetchProfile, fetchSkills } from "../data/sanity";
-import { Card, Image, Container, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import {
+  getImageUrl,
+  fetchProfile,
+  fetchSkills,
+  fetchGlobal,
+} from "../data/sanity";
 import TypingEffect from '../components/TypingEffect';
 
-// Refactor to pull on lifecycle hook and set to state
-const profile = await fetchProfile();
-const skills = await fetchSkills();
-
 export const HomePage = () => {
-  const profileImage = getImageUrl(profile.image).size(300, 300).url();
+  const [profile, setProfile] = useState(null);
+  const [skills, setSkills] = useState([]);
+  const [global, setGlobal] = useState(null);
+  const [profileImage, setProfileImage] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const profileData = await fetchProfile();
+      const skillsData = await fetchSkills();
+      const globalData = await fetchGlobal();
+
+      setProfile(profileData);
+      setSkills(skillsData);
+      setGlobal(globalData);
+
+      if (profileData?.image) {
+        setProfileImage(getImageUrl(profileData.image).size(300, 300).url());
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const mapListItems = (items) => {
-    return items.map((item) => <li>{item.title}</li>);
+    return items.map((item, index) => <li key={index}>{item.title}</li>);
   };
 
-  const skillsList = skills.map((skill) => {
+  const skillsList = skills.map((skill, index) => {
     return (
-      <Col>
+      <div key={index}>
         <h4>{skill.title}</h4>
         <ul>{mapListItems(skill.skills)}</ul>
-      </Col>
+      </div>
     );
   });
 
   return (
-    <main className='home page'>
-
-      <Card>
-        <Container className="card-container">
-          <Row>
-            <Col className="card-title">
-              <h2 className="text-center">About Me</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={6} md={4}>
-              <Image src={profileImage} thumbnail fluid />
-            </Col>
-            <Col>
-              <Row>
-                <Col>
-                  <TypingEffect text={profile.bio} />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <h3>Skills</h3>
-                </Col>
-              </Row>
-              <Row>{skillsList}</Row>
-            </Col>
-          </Row>
-        </Container>
-      </Card>
+    <main className='home page container'>
+      {profileImage && <img src={profileImage} alt="Profile" />}
+      {profile && <TypingEffect text={profile.bio} />}
+      {skillsList}
     </main>
   );
 };
